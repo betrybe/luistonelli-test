82f9de90d7aa01e28936abd86c622c1ac1067a3f
tiominga <tiominga@gmail.com>
2022-02-13 22:27:27 -0300

    Entrada de usuario ok

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const usersController = require('../controllers/users');
+const db = require('../models/db');
 
 const app = express();
 
-// Não remover esse end-point, ele é necessário para o avaliador
-app.get('/', (request, response) => {
+module.exports = app; // Não remover esse end-point, ele é necessário para o avaliador
+
+app.get('/', (request, response) => { // Não remover esse end-point, ele é necessário para o avaliador
   response.send();
 });
-// Não remover esse end-point, ele é necessário para o avaliador
 
-module.exports = app;
+app.use(express.json());
+
+const users = [];
+
+app.post('/users', usersController.getUsers);
+
+app.get('/recipes', (req, res) => { // cria uma rota para as receitas
+  res.send('receitas...');
+});
+
+app.listen(3000, () => {
+  console.log('Ta rolando...');
+});// nada depois do liste
